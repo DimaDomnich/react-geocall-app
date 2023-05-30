51f5a5a274124ed278536635a8ea6e8f70cb5063
Dima Domnich <dima.domnich.mail@gmail.com>
2023-05-30 18:58:13 +0300

    update local sound

--- front/src/VideoRooms/ParticipantsVideos.js
+++ front/src/VideoRooms/ParticipantsVideos.js
@@ -7,12 +7,11 @@ const ParticipantsVideos = () => {
   const inRoom = useSelector((state) => state.videoRooms.inRoom);
   const localStream = useSelector((state) => state.videoRooms.localStream);
   const remoteStream = useSelector((state) => state.videoRooms.remoteStream);
-  const isMicOn = useSelector((state) => state.videoRooms.isMicOn);
 
   return (
     <div className="map_page_v_rooms_videos_container">
       {inRoom && <VideoRoomButtons inRoom={inRoom} />}
-      {inRoom && localStream && <Video stream={localStream} muted={!isMicOn} />}
+      {inRoom && localStream && <Video stream={localStream} muted />}
       {inRoom && remoteStream && <Video stream={remoteStream} />}
     </div>
   );
